41d0c88e6434b67fcd8e9a28584aa27da8856dda
Nader <legandryeagle@gmail.com>
2017-08-04 23:05:39 +0200

    done with an option to have some fixes

--- src/App.js
+++ src/App.js
@@ -1,75 +1,136 @@
 import React, { Component } from 'react';
-import { Collapse,Dialog,Button } from 'element-react';
+import ReactDOM from 'react-dom';
+import { Collapse,Button  } from 'element-react';
 import 'element-theme-default';
-//import recipe from './sRecipe.js';
+import Recipe from './sRecipe.js';
 import './App.css';
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state= {}
+
     this.state.data = {
       recipes:[{
-        name:'Juice mix',
-       ingredients:['light','potatos']
+        name:"Juice mix",
+       ingredients:["light","potatos"]
      },{
-       name:'salat',
-       ingredients:['not much ','get it done please']
+       name:"salat",
+       ingredients:["not much","get it done please"]
      }]
     }
-
+this.state.renderC = false
     this.state.dialog = {
     form: {
       name: '',
       ingredients: []
-    }
-  };
+    },
+   title:'',
+   visible: false
+ };
+ this.state.actRecipe = 0
+ this.state.newAdd = false
 
   }
   render() {
-    let self = this
+    var self = this
     let activeName = this.state.data.recipes[0].name || undefined
-    let recipes = this.state.data.recipes.map(function(e){
-      let ingredients = e.ingredients.map(function(ele){
+    this.state.recipes = this.state.data.recipes.map(function(e,index){
+      var ke = e.index
+      let ingredients = e.ingredients.map(function(ele,inder){
         return(
-          <p key={e.ingredients.indexOf(ele)}>{ele}</p>
+          <p className="ingres" key={inder}>{ele}</p>
         )
       })
       return (
-        <Collapse.Item title={e.name} key={e.index} name={e.name}>
+        <Collapse.Item title={e.name} key={index} name={e.name}>
         {
          ingredients
         }
-        <Button type="primary" onClick={self.edit.bind(this,e.index)} >Edit</Button>
-        <Button type="primary" onClick={self.deleteRecipe.bind(this,e.index)} >Delete</Button>
+        <Button type="primary" onClick={()=>{
+          self.edit(index)
+        }} >Edit</Button>
+        <Button type="primary" onClick={()=>{self.deleteRecipe(index)}} >Delete</Button>
         </Collapse.Item>
       )
     })
+
     return (
-      <div className="App">
+      <div  id='app' className="App">
        <div className="recipes">
            <Collapse value={activeName}>
-              {recipes}
+              {this.state.recipes}
            </Collapse>
-           <Button onClick={this.add.bind(this)} >Add</Button>
+
        </div>
+    <div className="preview">
+
+          { this.state.renderC ?
+            <div className="compn">
+         <Recipe form={this.state.dialog.form} savCh={this.saveC.bind(this)} close={this.closeform.bind(this)} onChange={()=>{
+           console.log('hellio')
+
+         }}  />
+         <Button type="primary" className="editButton" onClick={this.addIngre.bind(this)} >add ingredient</Button>
+
+         </div>  : null
+   }
+      </div>
+
+   <Button onClick={this.add.bind(this)} className="read" >Add Recipe</Button>
       </div>
     );
   }
-  edit(){
-
+  saveC(){
+    console.log(this.state.dialog.form,'helllooodas')
+    this.state.data.recipes[this.state.dialog.form.index] = JSON.parse(JSON.stringify(this.state.dialog.form))
+    this.forceUpdate()
+    localStorage.setItem('user',JSON.stringify(this.state.data))
   }
-  deleteRecipe(){
+  addIngre(){
 
+    this.state.dialog.form.ingredients.push('')
+    console.log(this.state.dialog.form)
+    this.forceUpdate()
+  }
+  edit(index){
+let self = this
+
+//this.state.arc = <p>go</p>
+console.log('jhello', this.state.data,this.state.actRecipe,index)
+this.state.dialog.form = JSON.parse(JSON.stringify(this.state.data.recipes[index]))
+this.setState({renderC : true})
+//this.state.nameV =  this.state.data.recipes[this.state.actRecipe].name
+console.log(this.state.dialog.form)
+console.log(this.refs)
+  }
+  closeform(){
+    this.state.renderC =false
+    this.forceUpdate()
+  }
+  deleteRecipe(index){
+    this.state.data.recipes.splice(index,1)
+    this.forceUpdate()
+    localStorage.setItem('user',JSON.stringify(this.state.data))
   }
   add(){
-
+  this.state.data.recipes.push({
+    name:'',
+    ingredients:[],
+    index:this.state.data.recipes.length-1
+  })
+  this.forceUpdate()
   }
+
   componentDidMount(){
     let self = this
-    if(localStorage.getItem('user')){
+
+    if(JSON.parse(localStorage.getItem('user')).recipes.length){
+      console.log(localStorage.getItem('user'),'hey youoooo user here')
       this.state.data = JSON.parse(localStorage.getItem('user'))
+      this.forceUpdate()
     }
+    console.log('get it done')
     for(let ke in this.state.data.recipes){
       self.state.data.recipes[ke].index = ke
     }
--- src/sRecipe.js
+++ src/sRecipe.js
@@ -1,9 +1,125 @@
 import React, { Component } from 'react';
 
-class recipe extends Component{
+class Recipe extends Component{
   constructor(props){
     super(props)
-    
+    this.state  ={}
+    this.updateUser = this.updateUser.bind(this)
+    //this.ingreRemo = this.ingreRemo.bind(this)
+  }
+  render(){
+    let self = this
+    this.state.ingres = this.props.form.ingredients.map(function(e,index){
+      return(
+        <div key={index} className="ingres">
+        <input type='text' key={index} placeholder="ingredient"  ref={'ingr'+String(index)} />
+        <p className="closex" key={index+10} onClick={()=>{
+          self.ingreRemo(index)
+        }}>X</p>
+        </div>
+
+        )
+    })
+    return(
+      <div className="edform">
+        <label >Recipe name</label>
+      <input type="text" ref="name" />
+      <br/>
+    {this.state.ingres}
+    <br/>
+    <button onClick={this.save.bind(this)}>Save changes</button>
+    <br/>
+    <button onClick={this.close.bind(this)}>close</button>
+    <br/>
+      </div>
+    )
+  }
+  componentWillReceiveProps(nextProps) {
+    if(JSON.stringify(this.props.form) !== JSON.stringify(nextProps.form)){
+     this.props.form.name = nextProps.form.name
+     this.props.form.ingredients = []
+     for(let ke in  nextProps.form.ingredients){
+       this.props.form.ingredients.push(nextProps.form.ingredients[ke])
+     }
+     this.props.form.index = nextProps.form.index
+     console.log('hello')
+     this.updateUser()
+
+    }
+    console.log('why the heck')
+  }
+  close(){
+    this.props.close()
+  }
+  save(){
+    this.props.form.name = this.refs.name.value
+    this.props.form.ingredients = []
+    for(let he in this.refs){
+      if(he=='name'){}else{
+        console.log(he)
+        this.props.form.ingredients.push(this.refs[he].value)
+      }
+    }
+    this.props.savCh()
+    this.setState({rForm: this.props.form})
+console.log(this.props.form)
+  }
+  ingreRemo(index){
+  if(!Number.isNaN(index)){
+    //console.log('hekllafa')
+    this.props.form.ingredients.splice(index,1)
+    //console.log((this.props.form.ingredients))
+    this.updateUser()
   }
 }
-export default recipe;
+  updateUser() {
+    console.log(this)
+this.forceUpdate()
+let self = this
+this.state.ingArr = []
+for(let ke in this.props.form.ingredients){
+  this.state.ingArr.push(this.props.form.ingredients[ke])
+}
+//console.log(this.props.form.ingredients,this.refs)
+this.refs.name.value = this.props.form.name
+
+for(let ke = 0;ke< this.props.form.ingredients.length;ke++){
+  if(ke !== 1){
+  ///console.log(self.refs['ingr'+String(ke)],self.refs,'ingr'+String(ke),self.refs['ingr1'],"ingr1",ke)
+  self.refs['ingr'+String(ke)].value = self.state.ingArr[ke]
+ }
+else{
+  //console.log(self.refs['ingr'+String(ke)],'ingr'+String(ke),self.refs['ingr1'],"fhadaoigjjjj",ke)
+  //console.log(self.refs,self.refs['ingr1'])
+  setTimeout(function(){
+  self.refs['ingr1'].value= self.state.ingArr[ke]
+},200)
+}
+}
+this.forceUpdate()
+}
+  componentWillMount(){
+    let self = this
+    console.log(this.props,'hellllo its gets mounted')
+
+    this.state.ingArr = []
+    for(let ke in this.props.form.ingredients){
+      this.state.ingArr.push(this.props.form.ingredients[ke])
+    }
+    console.log(this.state.ingArr)
+  }
+  componentDidMount(){
+    this.updateUser()
+    let self = this
+    console.log(this.props.form.ingredients)
+    this.refs.name.value = this.props.form.name
+    for(let ke in this.props.form.ingredients){
+      self.refs['ingr'+String(ke)].value = self.state.ingArr[ke]
+    }
+  // this.setState({go : 0})
+  //  this.setState({go : go+1})
+this.forceUpdate()
+  }
+
+}
+export default Recipe;
